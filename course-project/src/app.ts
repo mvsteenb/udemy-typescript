57293d393b5a2a28bf66c95134ac9a960da315f1
Mario Van Steenberghe <m.vansteenberghe@Medix.local>
2021-07-10 22:38:31 +0200

    continue course

--- course-project/src/app.ts
+++ course-project/src/app.ts
@@ -50,6 +50,6 @@ const addedNumber3 = addNumbers3(10, 2, 8);
 const hobby1 = hobbies[0];
 const hobby2 = hobbies[1];
 
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+const [hobby3, hobby4, ...remainingHobbies] = hobbies;
 
 const { firstName: userNameElement, age } = personObject;
