64f247fd0cbd1ffd35e2d0194944bfc590f755b8
mario.vansteenberghe <mario.vansteenberghe@liantis.be>
2021-09-23 16:58:42 +0200

    3rd party libs

--- typescript-3d-party-libs/src/app.ts
+++ typescript-3d-party-libs/src/app.ts
@@ -1,1 +1,31 @@
-// Code goes here!
+// import _ from 'lodash';
+// console.log(_.shuffle([1, 2, 3]));
+
+// declare var GLOBAL: string;
+// console.log(GLOBAL);
+
+import {Product} from '../product.model';
+import 'reflect-metadata';
+import {plainToClass} from 'class-transformer';
+
+'class-transformer'
+
+const products = [
+    { title: 'A carpet', price: 29.99 },
+    { title: 'A book', price: 10.99 }
+];
+const p1 = new Product('A book', 12.99);
+console.log(p1.getInformation());
+
+/*const loadedProducts = products.map(p => {
+    return new Product(p.title, p.price);
+});
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}*/
+
+// using class-transformer
+const loadedProducts = plainToClass(Product, products);
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
