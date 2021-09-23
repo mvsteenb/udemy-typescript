ec20030d44f35ddf14acbaf34a5be4bd1b026335
Mario Van Steenberghe <mario.vansteenberghe@gmail.com>
2021-09-23 23:07:59 +0200

    implemented google maps api

--- place-app/src/app.ts
+++ place-app/src/app.ts
@@ -1,1 +1,48 @@
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+const GOOGLE_API_KEY = 'AIzaSyDFdHLewyCByTEsaFsPz1hQjNm-PdDTnFE';
+
+//declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: {geometry: {location: {lat: number, lng: number}}}[];
+  status: 'OK' | 'ZERO_RESULTS';
+}
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  const encodedAddress = encodeURI(enteredAddress);
+
+  // send address to Google API
+
+  axios
+      .get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${GOOGLE_API_KEY}`)
+      .then(response => {
+        if (response.data.status !== 'OK') {
+          throw new Error('Could not fetch location!');
+        }
+
+        console.log(response);
+        const coordinates = response.data.results[0].geometry.location;
+
+        const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+          center: coordinates,
+          zoom: 16,
+        });
+
+        new google.maps.Marker({
+          position: coordinates,
+          map: map,
+        });
+
+      })
+      .catch(err => {
+        alert(err.message);
+        console.log(err);
+      })
+}
+
+form.addEventListener('submit', searchAddressHandler);
